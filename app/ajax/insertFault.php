4f947d2971e0bbdd0e02bb39cf0965d573a33313
AskMeOff <wladislaw109@mail.ru>
2025-04-07 16:29:35 +0300

    вставка файла

--- app/ajax/insertFault.php
+++ app/ajax/insertFault.php
@@ -5,6 +5,32 @@ if (!$connectionDB) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
+function translit($fileName)
+{
+    $tr = array(
+        "А"=>"A","Б"=>"B","В"=>"V","Г"=>"G",
+        "Д"=>"D","Е"=>"E","Ж"=>"J","З"=>"Z","И"=>"I",
+        "Й"=>"Y","К"=>"K","Л"=>"L","М"=>"M","Н"=>"N",
+        "О"=>"O","П"=>"P","Р"=>"R","С"=>"S","Т"=>"T",
+        "У"=>"U","Ф"=>"F","Х"=>"H","Ц"=>"TS","Ч"=>"CH",
+        "Ш"=>"SH","Щ"=>"SCH","Ъ"=>"","Ы"=>"YI","Ь"=>"",
+        "Э"=>"E","Ю"=>"YU","Я"=>"YA","а"=>"a","б"=>"b",
+        "в"=>"v","г"=>"g","д"=>"d","е"=>"e","ж"=>"j",
+        "з"=>"z","и"=>"i","й"=>"y","к"=>"k","л"=>"l",
+        "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
+        "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"h",
+        "ц"=>"ts","ч"=>"ch","ш"=>"sh","щ"=>"sch","ъ"=>"y",
+        "ы"=>"yi","ь"=>"","э"=>"e","ю"=>"yu","я"=>"ya",
+        "Ё"=>"E","Є"=>"E","Ї"=>"YI","ё"=>"e","є"=>"e","ї"=>"yi",
+        " "=> "_", "/"=> "_"
+    );
+
+    $fileName = strtr($fileName, $tr);
+    $fileName = preg_replace('/[^A-Za-z0-9_\-.]/', '', $fileName);
+
+    return $fileName;
+}
+
 $date_fault = $_POST['date_fault'] ?? null;
 $date_call_service = $_POST['date_call_service'] ?? null;
 $reason_fault = $_POST['reason_fault'] ?? null;
@@ -27,7 +53,7 @@ if (!file_exists($documentsDir)) {
 
 $file_name = null;
 if (isset($_FILES['fileReport']) && $_FILES['fileReport']['error'] === UPLOAD_ERR_OK) {
-    $file_name = basename($_FILES['fileReport']['name']);
+    $file_name = translit(basename($_FILES['fileReport']['name']));
     $file_tmp = $_FILES['fileReport']['tmp_name'];
     move_uploaded_file($file_tmp, $documentsDir . $file_name);
 }
