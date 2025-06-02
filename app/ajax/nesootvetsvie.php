c8da69540d6ddb3c044056717b79f1fe53a84f0f
AskMeOff <wladislaw109@mail.ru>
2025-06-02 11:17:59 +0300

    правка статистики

--- app/ajax/nesootvetsvie.php
+++ app/ajax/nesootvetsvie.php
@@ -49,11 +49,24 @@ echo '<table border="1">
 </th>
 </thead>
 <tbody>';
+$counts = [
+    'Брестская область' => '',
+    'Витебская область' => '',
+    'Гомельская область' => '',
+    'Гродненская область' => '',
+    'Минская область' => '',
+    'Могилевская область' => '',
+    'Минск' => '',
+    'РНПЦ' => ''
+];
 $countNevernoe = 0;
 $sqlobl = 'select id_oblast, name from oblast';
 $resultobl = mysqli_query($connectionDB->con, $sqlobl);
+$i=0;
 while($row = $resultobl->fetch_assoc()){
+    $i=0;
     $count_obl = 0;
+    $oblast = $row['name'];
     echo '<tr><td style="font-size: 25px; font-weight: 800">'.$row['name'].'</td></tr>';
 $id_oblast = $row['id_oblast'];
         $sql_type = 'SELECT o.id_oborudovanie, uz.name as name_uz, t.name as type_name, o.model, ob.name as ob_name
@@ -122,12 +135,100 @@ from oborudovanie o
         $countNevernoe++;
         $count_obl++;
     }
+    $counts[$oblast] = $count_obl;
     echo '<tr><td>'.$count_obl.'</td></tr>';
+    $i++;
 }
 
 
 
 echo '</tbody>
 </table>';
+
 echo $countNevernoe;
-//?>
+
+echo '<table border="1" cellpadding="10" cellspacing="0">';
+echo '<tr><th>Область</th><th>Значение</th></tr>';
+
+foreach ($counts as $region => $value) {
+    echo '<tr>';
+    echo '<td>' . htmlspecialchars($region) . '</td>';
+    echo '<td>' . htmlspecialchars($value) . '</td>';
+    echo '</tr>';
+}
+
+echo '</table>';
+
+
+$sql_type = '
+    SELECT count(*) as all_count, ob.name 
+    FROM oborudovanie o 
+    inner JOIN uz u ON u.id_uz = o.id_uz
+    inner JOIN oblast ob ON ob.id_oblast = u.id_oblast
+    where o.status in (0,1,3)
+    GROUP BY ob.name 
+';
+$resultobl_type = mysqli_query($connectionDB->con, $sql_type);
+$data = [];
+$total_my_value = 0;
+$total_all_count = 0;
+
+if ($resultobl_type && mysqli_num_rows($resultobl_type) > 0) {
+    while ($row = mysqli_fetch_assoc($resultobl_type)) {
+        $regionName = $row['name'];
+        $allCount = (int)$row['all_count'];
+
+        // Получаем значение из $counts, если оно есть
+        $myValue = isset($counts[$regionName]) ? (int)$counts[$regionName] : 0;
+
+        // Считаем процент
+        $percent = ($allCount > 0) ? round(($myValue / $allCount) * 100, 2) : 0;
+
+        // Накапливаем суммы для итоговой строки
+        $total_my_value += $myValue;
+        $total_all_count += $allCount;
+
+        // Сохраняем данные для вывода
+        $data[] = [
+            'region' => $regionName,
+            'my_value' => $myValue,
+            'all_count' => $allCount,
+            'percent' => $percent
+        ];
+    }
+}
+
+// Вычисляем общий процент (если всего записей больше 0)
+$general_percent = ($total_all_count > 0)
+    ? round(($total_my_value / $total_all_count) * 100, 2)
+    : 0;
+
+// Теперь выводим таблицу
+echo '<table border="1" cellpadding="10" cellspacing="0">';
+echo '<tr><th>Область</th><th>Количество записей</th><th>Значение</th><th>Процент (%)</th></tr>';
+
+if (!empty($data)) {
+    foreach ($data as $item) {
+        echo '<tr>';
+        echo '<td>' . htmlspecialchars($item['region']) . '</td>';
+        echo '<td>' . htmlspecialchars($item['all_count']) . '</td>';
+        echo '<td>' . htmlspecialchars($item['my_value']) . '</td>';
+        echo '<td>' . htmlspecialchars($item['percent']) . '</td>';
+        echo '</tr>';
+    }
+
+    // Итоговая строка
+    echo '<tr style="font-weight:bold; background-color:#f2f2f2;">';
+    echo '<td>Всего</td>';
+    echo '<td>' . number_format($total_all_count, 0, '', ' ') . '</td>';
+    echo '<td>' . number_format($total_my_value, 0, '', ' ') . '</td>';
+    echo '<td>' . htmlspecialchars($general_percent) . '%</td>';
+    echo '</tr>';
+
+} else {
+    echo '<tr><td colspan="4">Нет данных для отображения.</td></tr>';
+}
+
+echo '</table>';
+
+?>
